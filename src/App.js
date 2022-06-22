fcdc65fca4066499c8d33e874bf0d0ddc4927e30
ShenQi1996 <qisam1989@gmail.com>
2022-06-22 16:36:50 -0400

    done with apis. done with like and reject, done with cost and time, done with localStorage save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState, useEffect } from "react";
+//APIs
+// import { fetchUser } from "./api/UserAPI.js";
+
+//bootstrap
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
+  const [startup, setStartup] = useState([]);
+
+  useEffect(() => {
+    fetchAll();
+  }, []);
+
+  const fetchAll = async () => {
+    let startupData = await fetchData().catch(console.error);
+    setStartup([{ startupData }]);
+  };
+
+  const getRandomNumberTime = (min = 1, max = 12) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const getRandomNumberCost = (min = 1, max = 1000) => {
+    return Math.floor(Math.random() * (max - min + 1) + min) / 100;
+  };
+
+  const fetchData = async () => {
+    const resUser = await fetch(`https://randomuser.me/api/?results=1`);
+    const dataUser = await resUser.json();
+    const resIdea = await fetch("https://itsthisforthat.com/api.php?json");
+    const dataIdea = await resIdea.json();
+    let results = dataUser.results;
+    let estimated = await getRandomNumberTime();
+    let cost = await getRandomNumberCost();
+    const data = { results, dataIdea, estimated, cost };
+    return data;
+  };
+
+  const storeStartup = obj => {
+    console.log(obj);
+    let size = localStorage.length;
+    console.log(localStorage.length);
+    localStorage.setItem(size, JSON.stringify(obj));
+  };
+
+  const handleReject = () => {
+    fetchAll();
+  };
+
+  if (startup.length === 0) {
+    return <div></div>;
+  }
+
+  console.log(startup);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="app d-flex justify-content-center">
+      <Row className="text-center align-items-center">
+        <Col xs={12}>
+          <Container>
+            <img src={startup[0].startupData.results[0].picture.large} alt="" />
+          </Container>
+        </Col>
+        <Col xs={12}>
+          <h4>
+            Name: {startup[0].startupData.results[0].name.title}{" "}
+            {startup[0].startupData.results[0].name.first} ,{" "}
+            {startup[0].startupData.results[0].name.last}
+          </h4>
+          <h4>Email: {startup[0].startupData.results[0].email}</h4>
+          <h5>Phone Number: {startup[0].startupData.results[0].phone}</h5>
+        </Col>
+        <Col xs={12}>
+          <h4>Estimated Time: {startup[0].startupData.estimated} Months.</h4>
+          <h5>Cost: {startup[0].startupData.cost} Million Dollars</h5>
+        </Col>
+        <Col xs={12}>
+          <h2>
+            It's like a {startup[0].startupData.dataIdea.this} for{" "}
+            {startup[0].startupData.dataIdea.that}.
+          </h2>
+        </Col>
+        <Col>
+          <button onClick={() => storeStartup(startup[0])}>Like</button>
+        </Col>
+        <Col>
+          <button onClick={() => handleReject()}>Reject</button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
